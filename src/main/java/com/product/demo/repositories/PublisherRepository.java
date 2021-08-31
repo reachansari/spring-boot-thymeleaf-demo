88c9de2eefcbc25b7a54bea4e08347f5cffa2572
Thameem <reachansari@gmail.com>
2021-08-31 16:00:31 +0800

    Update PublisherRepository.java

--- src/main/java/com/product/demo/repositories/PublisherRepository.java
+++ src/main/java/com/product/demo/repositories/PublisherRepository.java
@@ -2,7 +2,7 @@ package com.product.demo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.product.demo.model.Author;
+
 import com.product.demo.model.Publisher;
 
 public interface PublisherRepository extends CrudRepository<Publisher,Long> {
